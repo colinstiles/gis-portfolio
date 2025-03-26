c00fa1d43176773c066c9d894fb2aaccb18a0263
colinstiles <colin.stiles@kimley-horn.com>
2025-03-26 10:54:55 -0400

    update: js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 /*!
-    Title: Dev Portfolio Template
+    Title: GIS Portfolio
     Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
+    Last Change: 03/25/2025
+    Author: Ryan Fitzgerald, edited by Colin T. stiles
+    Repo: https://github.com/colinstiles/gis-portfolio
 */
 
 (function($) {
